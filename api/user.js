8fe4ed2b4a4a61298ae3ab3dc9011b7c244d94d5
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-08-28 04:52:36 +0700

    user.js api created

---
+++ api/user.js
@@ -0,0 +1,17 @@
+import { connectToDatabase } from "../lib/mongodb";
+
+export default async function handler(req, res) {
+    if (req.method == "GET") {
+        try {
+            let { db } = await connectToDatabase();
+            let results = await db.collection("user").find({}).toArray();
+
+            return res.status(200).json({ status: { code: 200, message: 'OK'}})
+        }   catch (error) {
+            return res.json({
+                message: new Error(error).message,
+                success: false,
+            });
+        }
+    }
+}
