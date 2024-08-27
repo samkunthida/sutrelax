c669a55325431e88d7086c00e1279d98b9f79d19
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-08-28 04:51:36 +0700

    string.js value update

--- string.js
+++ string.js
@@ -3,11 +3,13 @@ const stringTH = {
     createAccount: "สร้างบัญชีผู้ใช้",
     login: "เข้าสู่ระบบ",
     register: "สมัครสมาชิก",
-    forgetPS: "ลืมรหัสผ่าน",
+    forgetPassword: "ลืมรหัสผ่าน",
     email: "อีเมล",
-    password: "รหัสผ่าน"
+    password: "รหัสผ่าน",
+    passwordConfirm: "ยืนยันรหัสผ่าน",
 
-
+    //HomeScreen
+    home_greeting: "สวัสดี! คุณ "
 }
 
 export default stringTH;
