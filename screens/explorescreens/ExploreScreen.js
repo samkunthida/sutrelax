80ad62d9a9c544acd561f67c2388bf4ce4c721ee
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-09-10 23:02:50 +0700

    Create new files for screens

---
+++ screens/explorescreens/ExploreScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+//import factors
+import colors from '../../factors/colors'
+import stringTH from '../../factors/strings'
+
+export default function ExploreScreen() {
+  return (
+    <View>
+      <Text>Explore Screen</Text>
+    </View>
+  )
+}
