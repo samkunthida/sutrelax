85b268be213ddcefda55d1f2ce2a23fefb5c2afc
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-09-13 02:13:39 +0700

    Making Profile Screen

--- screens/profilescreens/ProfileScreen.js
+++ screens/profilescreens/ProfileScreen.js
@@ -1,10 +1,11 @@
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
 import React from 'react'
 
 
 //import factors
 import colors from '../../factors/colors'
 import stringTH from '../../factors/strings'
+import images from '../../factors/images'
 
 //import components
 import Button1 from '../../components/Button1'
@@ -14,19 +15,31 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const ProfileScreen = ({ navigation }) => {
 
+  const firstName = "สมชาย";
+  const lastName = "ใจดี";
+  const dateCreatedAccount = "1 มกราคม 2564";
+  const profileImage = images.PROFILEIMAGE;
+
   function handleSignOut() {
     AsyncStorage.setItem('isLoggedIn', '');
     AsyncStorage.setItem('token', '');
-    navigation.reset({
-      index: 0,
-      routes: [{ name: 'Splash' }],
-    });
+    navigation.navigate('Splash');
   }
 
   return (
     <View>
-      <Text>Profile Screen</Text>
+      <Image source={images.PROFILEBG} style={styles.backgroundImage1} />
+
+      <View style={styles.profileImageContainer}>
+        <Image source={profileImage} style={styles.profileImage} />
+      </View>
+
+      <Text>{firstName + " " + lastName}</Text>
+      <Text>{"เข้าร่วมเมื่อ " + dateCreatedAccount}</Text>
+
+      <View style={styles.buttonContainer}>
       <Button1 style={styles.button} text="ออกจากระบบ" onPress={handleSignOut} ></Button1>
+      </View>
     </View>
   )
 }
@@ -37,6 +50,30 @@ const styles = StyleSheet.create({
     },
     button: {
       backgroundColor: colors.sut_red
+    },
+    buttonContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 50
+    },
+    profileImageContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 125
+    },
+    profileImage: {
+      width: 116,
+      height: 116,
+      borderRadius: 100,
+      borderWidth: 3,
+      borderColor: colors.sut_white
+    },
+    backgroundImage1: {
+      resizeMode: "contain",
+    width: 420,
+    height: 925,
+    position: 'absolute',
+    top: -355,
     }
 });
 
