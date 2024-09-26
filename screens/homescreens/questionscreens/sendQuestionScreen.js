60213c8f7bc9f72afd17c0c2a5c00717b3f0c8eb
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-09-26 21:10:41 +0700

    Created three blank screen

---
+++ screens/homescreens/questionscreens/sendQuestionScreen.js
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet, Image, Dimensions, TextInput} from 'react-native'
+import React from 'react'
+import { useState } from 'react';
+
+//import factors
+import colors from '../../../factors/colors'
+import stringTH from '../../../factors/strings'
+
+//import components
+import MenuCard3 from '../../../components/MenuCard3';
+
+const SendQuestionScreen = ({ navigation }) => {
+
+    // Functions
+    const a = () => {
+        //navigation.navigate('a')
+    }
+
+    // Screen
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.sut_white,
+        alignItems: 'center',
+        alignContent: 'center'
+      }
+});
+
+export default SendQuestionScreen
