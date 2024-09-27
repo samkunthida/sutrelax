97f8776b8654e9b26d6a3aa31179e924552bc4b8
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-09-27 08:02:33 +0700

    Fixed headerTitle of ExploreStack

--- stacks/ExploreStacks.js
+++ stacks/ExploreStacks.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import colors from '../factors/colors';
+
 import screenRoutes from '../factors/screensRoutes';
 import ARTICLES from '../screens/explorescreens/ArticlesScreen';
+import ARTICLEDETAIL from '../screens/explorescreens/ArticleDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
 const ExploreStacks = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerTitleStyle: {
+              fontFamily: 'Kanit-Medium',
+              color: colors.sut_darkblue,
+          },
+          headerTitleAlign: 'center',
+          headerTintColor: colors.sut_darkblue,
+          headerStyle: { backgroundColor: colors.sut_white },
+      }}>
         <Stack.Screen
         name="ExploreScreen"
         component={screenRoutes.EXPLORE}
-        options={{ headerShown: false }}
+        options={{ headerShown: true, headerTitle: "สำรวจ" }}
       />
       <Stack.Screen
         name="ArticlesScreen"
         component={ARTICLES}
-        options={{ headerShown: false }}
+        options={{ headerShown: true, headerTitle: "บทความ" }}
+      />
+      <Stack.Screen
+        name="ArticleDetailScreen"
+        component={ARTICLEDETAIL}
+        options={{ headerShown: true, headerTitle: "บทความ" }}
       />
         </Stack.Navigator>
     );
