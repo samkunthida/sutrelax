d21d6045683f85b44e5875b1e887ac3590d57f20
sam_kunthida <132780967+el-gato-no-amando@users.noreply.github.com>
2024-10-19 11:54:02 +0700

    Prepared to send data to database on sendQuestionScreen

--- screens/homescreens/questionscreens/sendQuestionScreen.js
+++ screens/homescreens/questionscreens/sendQuestionScreen.js
@@ -6,21 +6,46 @@ import { useState } from 'react';
 import colors from '../../../factors/colors'
 import stringTH from '../../../factors/strings'
 
-//import components
-import MenuCard3 from '../../../components/MenuCard3';
+import Button1 from '../../../components/Button1';
 
 const SendQuestionScreen = ({ navigation }) => {
 
+    const [topic, setTopic] = useState('');
+    const [content, setContent] = useState('');
     // Functions
     const a = () => {
         //navigation.navigate('a')
     }
+    
+    const handleSend = () => {
+        console.log(topic, content);
+        const questionData = {
+          topic: topic,
+          content: content
+        }
+        axios
+          //.post("http://
+    }
+        
 
     // Screen
     return (
         <View style={styles.container}>
-            
+        <TextInput style={styles.textinput1}
+          placeholder={"หัวข้อ"}
+          placeholderTextColor={colors.sut_grey7d}
+          value={topic}
+          onChangeText={setTopic} />
+
+          <TextInput style={styles.textinput2}
+          placeholder={"ข้อความที่อยากถาม"}
+          placeholderTextColor={colors.sut_grey7d}
+          value={content}
+          onChangeText={setContent} />
+        <Button1 text={"ตั้งคำถาม"} onPress={"handleLogin"}></Button1>
+
         </View>
+        
     )
 }
 
@@ -29,7 +54,33 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.sut_white,
         alignItems: 'center',
-        alignContent: 'center'
+        alignContent: 'center',
+        paddingTop:20
+      },
+      textinput1: {
+        borderColor: colors.sut_grey7d,
+        borderWidth: 1,
+        height: 45,
+        width: 370,
+        borderRadius: 10,
+        paddingLeft: 15,
+        marginBottom: 15,
+        color: colors.sut_white,
+        fontFamily: 'Kanit-Regular',
+        fontSize: 16
+      },
+      textinput2: {
+        borderColor: colors.sut_grey7d,
+        borderWidth: 1,
+        height: 550,
+        width: 370,
+        borderRadius: 10,
+        paddingLeft: 15,
+        marginBottom: 15,
+        color: colors.sut_white,
+        fontFamily: 'Kanit-Regular',
+        fontSize: 16,
+        textAlignVertical: 'top'
       }
 });
 
